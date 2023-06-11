d6977e02ad3438cd27096378b3c7d22eecd2e95b
kingpin <monique.ahlheim@live.com>
2023-06-11 13:35:39 -0400

    set up README, html, added time function to javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timeDisplayEl = $('#currentDay');
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +32,8 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// calls function that displays current time
+displayTime();
+// allows the time to be updated every second
+setInterval(displayTime, 1000);
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# laughing-umbrella
-Module 5 Challenge: Work Day Scheduler
+# Module 5 Challenge: Work Day Scheduler
+
+# Description
+
+# Installation
+
+# Usage
+
+# Credits
+
+# License 
+
+Please refer to the license in the repo.
